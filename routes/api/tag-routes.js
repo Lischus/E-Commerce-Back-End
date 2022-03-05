06393e56c930d708cb359bf57f2acf9a5d7640cf
Martin Corr <martinjcorr2@gmail.com>
2022-03-05 17:06:01 -0500

    Fixed a bug in the Tag-Routes file

--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -68,26 +68,22 @@ router.put("/:id", async (req, res) => {
 });
 
 router.delete("/:id", async (req, res) => {
-  router.delete("/:id", async (req, res) => {
-    try {
-      const tagData = await Tag.destroy({
-        where: {
-          id: req.params.id,
-        },
-      });
+  try {
+    const tagData = await Tag.destroy({
+      where: {
+        id: req.params.id,
+      },
+    });
 
-      if (!tagData) {
-        res
-          .status(404)
-          .json({ message: "No library card found with that id!" });
-        return;
-      }
-
-      res.status(200).json(tagData);
-    } catch (err) {
-      res.status(500).json(err);
+    if (!tagData) {
+      res.status(404).json({ message: "No library card found with that id!" });
+      return;
     }
-  });
+
+    res.status(200).json(tagData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 module.exports = router;
